6b956a84df4cddc30e5bec6bf1696db88e6ba2c1
prashant-pathak-01 <prashantpathak6395@gmail.com>
2024-01-08 18:44:25 +0530

    ENV Passwords

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const connection=async()=>{
-    const url=process.env.MONGO_DB_URL;
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+
+    const url=`mongodb://${USERNAME}:${PASSWORD}@ac-40oqcxx-shard-00-00.1onuat0.mongodb.net:27017,ac-40oqcxx-shard-00-01.1onuat0.mongodb.net:27017,ac-40oqcxx-shard-00-02.1onuat0.mongodb.net:27017/?ssl=true&replicaSet=atlas-wgohlr-shard-0&authSource=admin&retryWrites=true&w=majority"
+`;
     try{
         await mongoose.connect(url,{useUnifiedTopology:true});
         console.log("Connected");
